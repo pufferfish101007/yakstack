b8915a3ebb91cc49e2463331499b40e1e673cb73
pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2025-07-11 15:25:03 +0100

    creating simple platformer using tutorial

--- rust/src/yak.rs
+++ rust/src/yak.rs
@@ -1,18 +1,19 @@
-use godot::classes::{ISprite2D, Input, InputEvent, Sprite2D};
+use godot::classes::{CharacterBody2D, ICharacterBody2D, Input, InputEvent};
+use godot::global::move_toward;
 use godot::prelude::*;
 
 #[derive(GodotClass)]
-#[class(base=Sprite2D)]
+#[class(base=CharacterBody2D)]
 struct Yak {
     speed: f64,
     angular_speed: f64,
 
-    base: Base<Sprite2D>,
+    base: Base<CharacterBody2D>,
 }
 
 #[godot_api]
-impl ISprite2D for Yak {
-    fn init(base: Base<Sprite2D>) -> Self {
+impl ICharacterBody2D for Yak {
+    fn init(base: Base<CharacterBody2D>) -> Self {
         godot_print!("Hello, world!"); // Prints to the Godot console
 
         Self {
@@ -26,52 +27,37 @@ impl ISprite2D for Yak {
         self.base_mut().set_process_unhandled_key_input(true);
     }
 
-    fn input(&mut self, event: Gd<InputEvent>) {
-        godot_print!("{}", event.to_string());
-
-        if event.is_action("ui_down") {
-            self.increase_speed(-2.0);
-            godot_print!("down");
-        }
-        if event.is_action("ui_up") {
-            self.increase_speed(2.0);
-            godot_print!("up");
-        }
-    }
-
     fn physics_process(&mut self, delta: f64) {
-        let input = Input::singleton();
+        let is_on_floor = self.base().is_on_floor();
 
-        // godot_print!("{:?}", self.base().get_process_mode());
+        let mut vel = self.base().get_velocity();
+        if !self.base().is_on_floor() {
+            let grav = self.base().get_gravity();
+            vel += grav * (delta as f32);
+        }
 
-        // godot_print!("{}", input.is_anything_pressed());
+        if Input::singleton().is_action_just_pressed("ui_up") && is_on_floor {
+            vel.y = Self::JUMP_VELOCITY;
+        }
 
-        // if input.is_action_pressed("ui_down") {
-        //     self.increase_speed(-2.0);
-        //     godot_print!("down");
-        // }
-        // if input.is_action_pressed("ui_up") {
-        //     self.increase_speed(2.0);
-        //     godot_print!("up");
-        // }
+        let direction = Input::singleton().get_axis("ui_left", "ui_right");
 
-        let radians = (self.angular_speed * delta) as f32;
-        self.base_mut().rotate(radians);
+        if direction != 0.0 {
+            vel.x = Self::SPEED * direction;
+        } else {
+            vel.x = move_toward(vel.x as f64, 0.0, Self::SPEED as f64 / 20.0) as f32;
+        }
 
-        let rotation = self.base().get_rotation();
-        let velocity = Vector2::RIGHT.rotated(rotation) * self.speed as f32;
-        self.base_mut().translate(velocity * delta as f32);
+        self.base_mut().set_velocity(vel);
+
+        self.base_mut().move_and_slide();
     }
 }
 
 #[godot_api]
-impl Yak {
-    #[func]
-    fn increase_speed(&mut self, amount: f64) {
-        self.speed += amount;
-        self.base_mut().emit_signal("speed_increased", &[]);
-    }
+impl Yak {}
 
-    #[signal]
-    fn speed_increased();
+impl Yak {
+    const JUMP_VELOCITY: f32 = -600.0;
+    const SPEED: f32 = 300.0;
 }
